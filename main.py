9ecb632698b671d03052c7e77e67d68d7c374756
Arun Kumar M <arun29.7.2003@gmail.com>
2024-09-02 01:45:25 +0530

    changes

--- main.py
+++ main.py
@@ -49,5 +49,5 @@ def detect():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
