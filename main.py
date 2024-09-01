045c0e95cf2b7ec0fac40beeb29eebda17a40ebb
Arun Kumar M <arun29.7.2003@gmail.com>
2024-09-02 00:57:49 +0530

    changes

--- main.py
+++ main.py
@@ -44,5 +44,10 @@ def detect():
         # Return error message if something goes wrong
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
